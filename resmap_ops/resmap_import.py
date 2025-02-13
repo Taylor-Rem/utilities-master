46c183c1921b8ec276419e550eb563e02109d74b
Taylor Remund <taylorremund@Taylors-MacBook-Pro.local>
2025-02-13 16:43:33 -0700

    created new class for resmap imports to be re-used by different jobs. Also simplified thread

---
+++ resmap_ops/resmap_import.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.common.by import By
+
+class ResmapImport:
+    def __init__(self, browser, thread):
+        self.browser = browser
+        self.thread = thread
+
+    def import_file(self, url, dropdown_values, file_path):
+        self.browser.driver.get(url)
+        self.browser.wait_login()
+        dropdowns = [{
+                'value': '//div[@class="flex-row card-text"]//details[@class="auto_complete"]',
+                'key': f'//li[normalize-space(text())="{dropdown_values[0]}"]'
+            },{
+                'value': '//div[@class="alert-info full-width"]//details[@class="auto_complete"]', 
+                'key': f'//li[normalize-space(text())="{dropdown_values[1]}"]'
+            }]
+        for dropdown in dropdowns:
+            if self.thread.is_cancelled:
+                break
+            self.browser.wait_click(By.XPATH, dropdown['value'])
+            self.browser.wait_for_presence_of_element(By.CSS_SELECTOR, 'ul.popup')
+            self.browser.wait_click(By.XPATH, dropdown['key'])
+
+        if self.thread.is_cancelled:
+            return
+        self.browser.wait_for_downloads()
+        self.browser.wait_for_presence_of_element(By.CSS_SELECTOR, 'input[type="file"]').send_keys(file_path)
+        self.browser.wait_for_load()
+        if self.thread.is_cancelled:
+            return
+        self.browser.wait_click(By.XPATH, '//div[contains(@class, "flex-row alert-info full-width")]/button[@type="button" and contains(@class, "primary push_button")]')
+        self.browser.wait_for_load()
